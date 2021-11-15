f7bfbc085208573bb47ed1e5c33f8dd39292e087
kristi80 <91655002+kristi80@users.noreply.github.com>
2021-11-15 11:17:08 +0100

    Update Milepaerl_0_prosjekt.py
    Tatt bort Martin sine farge verdier, og lagt til et globalt bibliotek med farger (alle fargene til Martin er som de var).

--- Milepaerl_0_prosjekt.py
+++ Milepaerl_0_prosjekt.py
@@ -13,6 +13,120 @@ sense = SenseHat()
 ROWS = 8
 COLS = 8
 
+# Farger
+r = (255, 0, 0)
+g = (0, 255, 0)
+b = (0, 0, 255)
+c = (0, 180, 255)
+m = (255, 0, 199)
+y = (120, 255, 0)
+k = (0, 0, 0)
+w = (255, 255, 255)    
+
+sense.set_rotation(90)
+
+#genererer rader med valgt farge fra høyre
+def paintRow(x, color):
+  #tegner først et bilde med valgt bakgrunnsfarge
+  array = []
+  u = 0
+  while u < 64:
+    array.append(r)
+    u += 1
+  i = 0
+  #fyller rader med valgt farge og antall rader
+  while i < 8*x:
+    array[i] = color
+    i += 1
+  sense.set_pixels(array)
+  
+#genererer rader med valgt farge fra venstre
+def paintRowLeft(x, color):
+  #tegner først et bilde med valgt bakgrunnsfarge
+  array = []
+  u = 0
+  while u < 64:
+    array.append(r)
+    u += 1
+  i = 63
+  #fyller rader med valgt farge og antall rader
+  while i > 63 - 8*x  :
+    array[i] = color
+    i -= 1
+  sense.set_pixels(array)
+
+  
+#henter verdier
+def getIMUValues():
+    # Gather all three sensor values from IMU
+    compass = sense.get_compass()
+    gyro = sense.get_gyroscope()
+    accel = sense.get_accelerometer()
+    
+    return {"compass":compass, "accel":accel, "gyro":gyro}
+  
+
+def funksjonen_til_kristian():
+    
+  #diff er differansen mellom nye rader
+  diff = 2
+  var = 360 #hvilket utgangspunkt du vil ha
+  konstant_deg = 5
+  #velg hvor lenge while løkka skal kjøre:
+  tid = 5
+  
+  #teller 60 sekunder x antall ganger
+  t_end = time.time() + 60*tid
+  i = 0 #brukes for å printe compass mindre
+  while time.time() < t_end:
+    IMUValues = getIMUValues()
+    compass = round(IMUValues["compass"])
+    accel = (IMUValues["accel"])
+    gyro = (IMUValues["gyro"]["pitch"])
+    i += 1
+    if i % 10 == 0:
+      print("\r", gyro, end="")
+    #nedover rot er ca. mellom 345 - 360
+    if gyro <= (var) and gyro > (var - diff):
+      paintRow(0, w)
+    elif gyro <= (var - diff) and gyro > (var - (diff*2)):
+      paintRow(1, w)
+    elif gyro <= (var - (diff*2)) and gyro > (var - (diff*3)):
+      paintRow(2, w)
+    elif gyro <= (var - (diff*3)) and gyro > (var - (diff*4)):
+      paintRow(3, w)
+    elif gyro <= (var - (diff*4)) and gyro > (var - (diff*5)):
+      paintRow(4, w)
+    elif gyro <= (var - diff*5) and gyro > (var - (diff*6)):
+      paintRow(5, w)
+    elif gyro <= (var - (diff*6)) and gyro > (var - (diff*7)):
+      paintRow(6, w)
+    elif gyro <= (var - (diff*7)) and gyro > (var - (diff*8)):
+      paintRow(7, w)
+    elif gyro <= (var - (diff*8)) and gyro > (var - (diff*9)):
+      paintRow(8, w)
+    #oppover rot er ca mellom: 0 - 45
+    elif gyro >= (0) and gyro < (konstant_deg):
+      paintRowLeft(0, w)
+    elif gyro >= (konstant_deg) and gyro < (konstant_deg + diff):
+      paintRowLeft(1, w)
+    elif gyro >= (konstant_deg + diff) and gyro < (konstant_deg + diff*2):
+      paintRowLeft(2, w)
+    elif gyro >= (konstant_deg + diff*2) and gyro < (konstant_deg + diff*3):
+      paintRowLeft(3, w)
+    elif gyro >= (konstant_deg + diff*3) and gyro < (konstant_deg + diff*4):
+      paintRowLeft(4, w)
+    elif gyro >= (konstant_deg + diff*4) and gyro < (konstant_deg + diff*5):
+      paintRowLeft(5, w)
+    elif gyro >= (konstant_deg + diff*5) and gyro < (konstant_deg + diff*6):
+      paintRowLeft(6, w)
+    elif gyro >= (konstant_deg + diff*6) and gyro < (konstant_deg + diff*7):
+      paintRowLeft(7, w)
+    elif gyro >= (konstant_deg + diff*7) and gyro < (konstant_deg + diff*8):
+      paintRowLeft(8, w)
+    else:
+      sense.clear()
+
 
 def gunnar():
     ### Conway's Game of Life ###
@@ -83,13 +197,6 @@ def gunnar():
 
 
 def martin():
-    r = (255, 0 , 0)
-    g = (0, 255, 0)
-    b = (0, 0, 255)
-    
-    c = (0, 180, 255)
-    m = (255, 0, 199)
-    y = (120, 255, 0)
     
     available_colors = [r, g, b, c, m, y]
     
@@ -117,7 +224,7 @@ def main():
             #andre()
             humidity = gunnar()
             #knut_ola()
-            #kristian()
+            funksjonen_til_kristian()
             compass = martin()
             
             # Sensor values to be written in sensor_values.csv
