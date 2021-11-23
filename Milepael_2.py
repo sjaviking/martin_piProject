11ac9b3131ac03a9806b0e2364ba90dc3114e462
raflemakt <raflemakt@hotmail.com>
2021-11-23 08:56:42 +0100

    Programmet kjører i det hele tatt B-)

--- Milepael_2.py
+++ Milepael_2.py
@@ -1,7 +1,23 @@
-from random import randint
-from sense_hat import SenseHat
+import random
+import time
+import os
+#from sense_hat import SenseHat
 
-sense = SenseHat()
+#sense = SenseHat()
+
+ROWS = 8
+COLS = 8
+
+FPS = 25
+FRAME_DURATION = 1 / FPS
+
+GATE_FREQUENCY = 8
+GATE_WIDTH = 2
+CAR_Y_POS = 6
+GAME_LENGTH = 200
+CAR_COLOR = (255, 255, 255)
+GATE_COLOR = (255, 0, 0)
+NOCOLOR = (0, 0, 0)
 
 
 def get_gate_pos():
@@ -9,8 +25,8 @@ def get_gate_pos():
     #Gjør så det er større sannsynlighet for å treffe porter i midten
     skew = lambda x: - (1/16) * (x - 4)**2 + 1
 
-    port_pos = skew(random.randint(0, 99) / 100) 
-    pass
+    port_pos = round(skew(random.randint(0, 99) / 100) * 8)
+    return port_pos
 
 
 def increment_buffer(buffer):
@@ -35,63 +51,75 @@ def get_imu_values():
 
 def calculate_car_position(imu_values):
     """Returner x-posisjon for bilen"""
-    pass
+    # returner tall mellom 0 og 7
+    return 3
 
 
 def debug_print(buffer):
+    os.system("clear")
     for line in buffer:
-        print(line)
+        for char in line:
+            if char == NOCOLOR:
+                print("..", end="")
+            if char == CAR_COLOR:
+                print("XX", end="")
+            if char == GATE_COLOR:
+                print("OO", end="")
+        print()
+    print()
 
 
 def main():
     running = True
     iterator = 0
     score = 0
-    buffer = []
+    #buffer = []
 
-    ROWS = 8
-    COLS = 8
-
-    GATE_FREQUENCY = 8
-    GATE_WIDTH = 4
-    CAR_Y_POS = 1
-    GAME_LENGTH = 120
-    CAR_COLOR = (255, 255, 255)
 
     intro_graphic()
 
     #Spillet starter
     while running:
+        buffer = [[NOCOLOR for x in range(8)] for y in range(8)]
         #Finn nye gyro-verdier for xyz
         imu_values = get_imu_values()
 
         #Finn ut hvor bilen skal stå
         car_x_pos = calculate_car_position(imu_values)
 
-        #TODO: Legg bilen til i printebuffer
+        #Legg bilen til i printebuffer
         buffer[CAR_Y_POS][car_x_pos] = CAR_COLOR
         
         #Etter "GATE_FREQUENCY" iterasjoner, lag en ny gate
         if iterator % GATE_FREQUENCY == 0:
             gate_x_pos = get_gate_pos()
             gate_y_start = iterator
+            print("Made gate")
 
-        #TODO: Legg gaten til i printebuffer
-        # x = gate_x_pos
+        #Legg gaten til i printebuffer
+        print(gate_x_pos)
         gate_y_pos = abs(iterator - gate_y_start)
-
-        #Inkrementer iterator
-        iterator += 1
+        buffer[gate_y_pos][gate_x_pos] = GATE_COLOR
+        buffer[gate_y_pos][gate_x_pos + GATE_WIDTH] = GATE_COLOR
 
         #Når bilen passerer en gate, sjekk om du traff
         if CAR_Y_POS == gate_y_pos:
             if gate_x_pos <= car_x_pos <= gate_x_pos + GATE_WIDTH:
                 score += 1
 
+        #Inkrementer iterator
+        iterator += 1
+
+        #Print banen
+        debug_print(buffer)
+
         #Når det har gått GAME_LENGTH antall iterasjoner, stopp spillet
         if iterator >= GAME_LENGTH:
             game_over_graphics(score)
             running = False
+
+        #Delay
+        time.sleep(FRAME_DURATION)
 
 
 if __name__ == "__main__":
