33be63b73507c548f71147cdcbd8d3da3a55d7b5
Erexotv <91662127+Erexotv@users.noreply.github.com>
2021-12-06 12:29:20 +0100

    Temperatur v2
    Forbedret temperatur koden, og la til kommentarer

--- Milepaerl_0_prosjekt.py
+++ Milepaerl_0_prosjekt.py
@@ -33,162 +33,112 @@ WHITE = (255, 255, 255)
 
 
 def aleksander():
-    pixel_list = sense.get_pixels()
-    sense.set_rotation(270)
-    W = (255, 255, 255)
-    B = (0, 0, 0)
-    p = (51, 0, 102)
-    b = (0, 0, 255)
-    y = (255, 255, 0)
-    o = (255, 69, 0)
-    r = (255, 0, 0)
+  W = (255, 255, 255) # White
+  B = (0, 0, 0)       # Black
+  p = (51, 0, 102)    # Purple
+  b = (0, 0, 255)     # Blue
+  y = (255, 255, 0)   # Yellow
+  o = (255, 69, 0)    # Orange
+  r = (255, 0, 0)     # Red
 
-    def getTemperature():
-        temperature = round(sense.get_temperature())
-        if temperature <= 31:
-            back_colour = p
-        elif 31 < temperature <= 32:
-            back_colour = b
-        elif 32 < temperature <= 33:
-            back_colour = y
-        elif 33 < temperature <= 34:
-            back_colour = o
-        elif temperature > 34:
-            back_colour = r
-        
-        speed = 0.05
-        sense.show_message("The Temp is: " + str(temperature) + "C", speed,
-                text_colour = W, back_colour =  back_colour)
-        return temperature
-
-    temperature = getTemperature()
-    myImage = [
-          B, B, W, W, W, W, B, B,
-          B, B, W, B, B, W, B, B,
-          B, B, W, B, B, W, B, B,
-          B, B, W, B, B, W, B, B,
-          B, B, W, B, B, W, B, B,
-          B, W, B, B, B, B, W, B,
-          B, W, B, B, B, B, W, B,
-          B, B, W, W, W, W, B, B,
-          ]
-    if temperature <= 31:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-    elif 31 < temperature <= 32:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for c in range(35,37):
-            myImage[c] = b    
+  def getTemperature():
+      # Henter temperaturverdi fra Senshat
+      temperature = round(sense.get_temperature())
       
-    elif 32 < temperature <= 33:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for c in range(35,37):
-            myImage[c] = b
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for v in range(27,29):
-            myImage[v] = y
-          
-    elif 33 < temperature <= 34:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for c in range(35,37):
-            myImage[c] = b
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for v in range(27,29):
-            myImage[v] = y
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for i in range(19,21):
-            myImage[i] = o
-          
-    elif 34 < temperature <= 39:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for c in range(35,37):
-            myImage[c] = b
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for v in range(27,29):
-            myImage[v] = y
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for i in range(19,21):
-            myImage[i] = o
-        
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for n in range(11,13):
-            myImage[n] = r
-          
-    elif temperature >= 40:
-        for z in range(50,54):
-            myImage[z] = p
-        for x in range(42,46):
-            myImage[x] = p
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for c in range(35,37):
-            myImage[c] = b
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for v in range(27,29):
-            myImage[v] = y
-          
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for i in range(19,21):
-            myImage[i] = o
-        
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for n in range(11,13):
-            myImage[n] = r
-        
-        sense.set_pixels(myImage)
-        time.sleep(1)
-        for n in range(2,6):
-            myImage[n] = r
-            myImage[9] = r
-            myImage[14] = r
-            myImage[16] = r
-            myImage[22] = r
-          
-    sense.set_pixels(myImage)
-    time.sleep(3)
+      # Gir melding en bakgrunnsfarge, avhengig av temperaturen
+      if temperature <= 31:
+          back_colour = p
+      elif 31 < temperature <= 32:
+          back_colour = b
+      elif 32 < temperature <= 33:
+          back_colour = y
+      elif 33 < temperature <= 34:
+          back_colour = o
+      elif temperature > 34:
+          back_colour = r
+       
+      # Printer ut temperaturen med bakgrunnsfarge
+      speed = 0.05
+      sense.show_message("The Temp is: " + str(temperature) +
+                           "C", speed, text_colour=W, back_colour=back_colour)
+      
+      return temperature  # Returnerer temperaturen
+    
+  # Setter temperaturen til get temperaturen for å ta med temperatur verdien videre
+  temperature = getTemperature()
+  
+  # Tegner termostaten
+  myImage = [
+      B, B, W, W, W, W, B, B,
+      B, B, W, B, B, W, B, B,
+      B, B, W, B, B, W, B, B,
+      B, B, W, B, B, W, B, B,
+      B, B, W, B, B, W, B, B,
+      B, W, B, B, B, B, W, B,
+      B, W, B, B, B, B, W, B,
+      B, B, W, W, W, W, B, B,
+      ]
+
+  def plot_ranges(color):
+      # Definerer fargene til en piksel posisjon
+      color_ranges = {
+          p: [(50, 54), (42, 46)],
+          b: [(35, 37)],
+          y: [(27, 29)],
+          o: [(19, 21)],
+          r: [(11, 13)],
+      }
+      # Bytter ut pixlene i termostaten med hvilken farge som skal vises
+      for (start, end) in color_ranges[color]:
+          for i in range(start, end):
+              myImage[i] = color
+      
+      # Bildet opptater seg for hver farge som skal vises
+      sense.set_pixels(myImage)
+      time.sleep(1)
+
+  # Sier hvilken farger som skal vises, avhengig av hvilken temperatur det er
+  if temperature <= 31:
+      plot_ranges(p)
+
+  elif 31 < temperature <= 32:
+      plot_ranges(p)
+      plot_ranges(b)
+
+  elif 32 < temperature <= 33:
+      plot_ranges(p)
+      plot_ranges(b)
+      plot_ranges(y)
+
+  elif 33 < temperature <= 34:
+      plot_ranges(p)
+      plot_ranges(b)
+      plot_ranges(y)
+      plot_ranges(o)
+
+  elif 34 < temperature <= 39:
+      plot_ranges(p)
+      plot_ranges(b)
+      plot_ranges(y)
+      plot_ranges(o)
+      plot_ranges(r)
+
+  elif temperature >= 40:
+      plot_ranges(p)
+      plot_ranges(b)
+      plot_ranges(y)
+      plot_ranges(o)
+      plot_ranges(r)
+      for n in range(2, 6):
+          myImage[n] = r
+      myImage[9] = r
+      myImage[14] = r
+      myImage[16] = r
+      myImage[22] = r
+  
+  # Setter bildet til slutt
+  sense.set_pixels(myImage)
+  time.sleep(3)
 
 
 def kristian():
