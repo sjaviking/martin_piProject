d31491b671ac344fab4894097e58d71ba6edd4ca
sjaviking <93977052+sjaviking@users.noreply.github.com>
2021-12-08 14:22:33 +0100

    Update Milepaerl_1.py

--- Milepaerl_1.py
+++ Milepaerl_1.py
@@ -1,183 +1,105 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 16 08:36:51 2021
 
-@author: Martin
-"""
-
+# henter inn ulike bibliotek for bruk i koden
 from sense_hat import SenseHat
 from time import sleep
-
-senseHat = SenseHat()
-
-# Konstanter
-a = -0.0065
-R = 287.06
-g_0 = 9.81
-N = 200
-
-#Variabler
-T1 = 16 + 273.15
-p_1 = 100000 #Trykket målt ved høyden h_1
-h_1 = 0 #Vi gjør målingene relative dersom vi ønsker ved a sette h_1 = 0
-
-def calc_pressure_Pa():
-    p = a
-    for _ in range (N):
-        p += senseHat.pressure
-        sleep(0.001)
-    return p*100/N #Vi vil ha p i Pa, og multipliserer derfor med 100 etter vi deler på N
-
-
-def stick_down():
-    for e in senseHat.stick.get_events() :
-
-        if e.direction=="down":
-            return 1
-
-while True:
-    senseHat.show_message ("Venstre: maal - Hoyre: kalibrer - Ned: avbryt",0.05)
-
-    event = senseHat.stick.wait_for_event()
-
-    if event.direction == "left":
-        while stick_down() != 1:
-            p = calc_pressure_Pa()
-            h = (T_1/a) * ( (p/p_1)**(-a*R/g_0) - 1) + h_1
-            senseHat.show_message(str(round(h,2) )+ "m" ,0.07)
-    if event.direction == "right":
-        while stick down()) !=1:
-            T_1 = senseHat.temp + 273.15
-            p_1 = calc_pressure_Pa()
-            senseHat.show_message(str(int (p_1))+" Pa "+str(T_1)+" K", 0.07)
-
-
-# Ny kodefil
-
-def write_to_log(write_data):
-    with open(path, 'a') as f:
-        f.write(str(write_data) + '\n')
-
-
-# Ny kodefil
-
-from sense_hat import SenseHat
-
-senseHat =()
-def draw():
-    #Tegn hvilken etasje du er på
-
-def calcEtasjer():
-    # Konstanter
-    a = -0.0065
-    R = 287.06
-    g_0 = 9.81
-    
-    # Variabler
-    T_1 = 16 + 237.15
-    p_1 = #Trykket målt i første etasje
-    h_1 = 0 # Starthøyden (første etasje) er definert som 0
-    h_e = # Høyden mellom etasjene
-    
-    p = senseHat.pressure * 100
-    
-    h = Hypsometrisk ligning
-    
-    etasje = h/h_e
-    toppetasje = 14
-    
-    if etasje < 0:
-        etasje = 0
-    elif etasje > toppetasje:
-        etasje = toppetasje
-    return int(etasje)
-
-while True:
-    draw(calcEtasjer())
-    
-    
-    
-# Ny kodefil
-from sense_hat import SenseHat
-from time import sleep, time
 from datatime import datatime as dt
 
-senseHat = SenseHat()
 
-# Konstanter
-a = -0.0065
-R = 287.06
-g_0 = 9.81
-N = 200
-path = "/home/pi/Documents" # Skriv inn din egen mappe du ønsker dataene skal havne i
-path = path.rstrip('/') + 'hoydedata.csv' # formaterer path riktig for å få filtypen vi vil ha
-
-# Variabler
-T_1 = 16 + 273.25
-p_1 = 100000 #trykket målt ved høyden h_1
-h_1 = 0 # vi gjør målingene relative dersom vi ønsker ved å sette h_1  = 0
-
-def write_to_log(write_data):
-    with open(path, 'a') as f:
-        f.write(str(write_data) + '\n')
-
-def calc_pressure_Pa():
-    p = 0
-    for _ in range (N):
-        p += senseHat.pressure
-        sleep(1/N)
-    return p*100/N #Vi vil ha p i Pa, og multipliserer derfor med 100 etter vi deler på N
-    
-def stick_down():
-    for e in senseHat.stick.get_events() :
-
-        if e.direction=="down":
-            return 1
-
-while True:
-    senseHat.show_message ("Venstre: maal - Hoyre: kalibrer - Ned: avbryt",0.05)
-
-    event = senseHat.stick.wait_for_event()
-
-    if event.direction == "left":
-        start = time()
-        
-        write_to_log('\n\n### New measurements started at: ' + datatime.now().strftime('%d%m%Y %H:%M:%S' + ' ###\n'))
-
-        while stick_down() !=1:
-            p = calc_pressure_Pa()
-            h = (T 1/a) * ( (p/p_1)**(-a*R/g_0) - 1) + h_1
-            
-            now = time() - start
-            
-            write_to_log(str(now) + ',' + str(p) + ',' + str(h) + ',' + str(senseHat.temp))
-
-            senseHat.show_message(str(round(h,2)) + 'm', 0.03)
-
-    if event.direction == "right":
-        while stick down()) !=1:
-            T_1 = senseHat.temp + 273.15
-            p_1 = calc_pressure_Pa()
-            senseHat.show_message(str(int (p_1))+" Pa "+str(eound(T_1))+" K", 0.03)
-
-
-# Ny kodefil
-form sense_hat import SenseHat # Import av SenseHat-funksjonen fra sense_hat modulen
-
-senseHat = SenseHat() # Dannelse av et SenseHat-objekt
+senseHat = SenseHat()  # Dannelse av et SenseHat-objekt
 
 # Konstanter
 a = -0.0065 # Temperaturgradient (Hvor mange grader kaldere luften blir per meter)
 R = 287.06# Den spesifikke gasskonstant (for tørr luft)
 g_0 = 9.81 # tyngdeakselerasjonen ved havnivå (omtrent)
+N = 200
+path = "/home/pi/Documents" # path vi ønsker at filen skal havne på
+path = path.rstrip('/') + 'hoydedata.csv' # formaterer path riktig for å få filtypen vi vil ha
 
-# variabler
-T_1 = 16 + 273.25 #Temperaturen målt ved h_1 og konvertert til kelvin
-p_1 = 100000 #trykket målt ved høyden h_1
-h_1 = 50 # Høyden målt ved bakkenivå ved hovedbygget til NTNU er 45 meter. Det er 50 ved realfagsbygget. vi tar 50 fordi vi sannsynligvis er litt over bakken der vi sitter. Sett verdien til null for å gjøre målinger relative til måleområdet
 
-while True:
-    p = senseHat.pressure * 100 # Lagrer trykket i Pa i stede for hPa som en variabel
-    h = (T 1/a) * ( (p/p_1)**(-a*R/g_0) - 1) + h_1 # Den hypsometriske likningen
+#Variabler
+T1 = 16 + 273.15 #Temperaturen målt ved h_1 og konvertert til kelvin
+p_1 = 100000 #Trykket målt ved høyden h_1
+h_1 = 0 #Vi gjør målingene relative
+''' Høyden målt ved bakkenivå ved hovedbygget til NTNU er 45 meter. Det er 50 ved realfagsbygget. 
+Vi tar 50 fordi vi sannsynligvis er litt over bakken der vi sitter. 
+Sett verdien til null for å gjøre målinger relative til måleområdet
+'''
+
+# funksjon som kalkulerer trykk og returnerer resultat i Pa
+def calc_pressure_Pa():
+    p = a  # trekker fra temperaturgradient - kan settes til 0
+    for _ in range (N):  # range satt til N = 200
+        p += senseHat.pressure  # legger til trykket på variabel p for hver kjøring av løkke
+        sleep(0.001)  # 0.001s delay før progarm kjører videre
+    return p*100/N #Vi vil ha p i Pa, og multipliserer derfor med 100 etter vi deler på N
+
+
+# Funksjon ved event stick_down
+def stick_down():
+    for e in senseHat.stick.get_events() :
+
+        if e.direction=="down":
+            return 1
+     
+# Funksjon for skriving av data til log-fil
+def write_to_log(write_data):
+    # Bruker with open for automatisk lukking, settes til a for append
+    with open(path, 'a') as f:
+        # skriver write_data variabel til log-fil og slutter med linjeskift
+        f.write(str(write_data) + '\n')
+        
+'''
+# Tegning av etasje i egen funskjon
+def draw(etasje):
+    message = 'etg ' +  etasje  # lager string av etasje
+     senseHat.show_message (message,0.05)  # printer string på HAT med speed 0.5
+     
+# funksjon som kalkulerer etasjer     
+def calcEtasjer():
+    h_e = 3 # Høyden mellom etasjene, satt til 3m da vi ikke vet nøyaktig
+    p_e = senseHat.pressure * 100 # live trykke variabel
     
-    senseHat.show_message(str(round(h,2)) + 'm', 0.008) # Skriver resultatet til Sense HAT
+    h_l = (T_1/a) * ( (p_e/p_1)**(-a*R/g_0) - 1) + h_1 # beregner høyde med hypsometrisk ligning
+   
+    etasje = h_l/h_e  # Kalkulerer etasje ved å dele høyde i moh på høyde til etasje
+    toppetasje = 14  # Satt maks antall etasjer i forhold til sentralbygget på Gløs
+    
+    if etasje < 0:
+        etasje = 0
+    elif etasje > toppetasje:
+        etasje = toppetasje      
+  '''     
+        
+while True:
+    # Show message viser bruker hvordan PI styres
+    senseHat.show_message ("Venstre: maal - Hoyre: kalibrer - Ned: avbryt",0.05)
+
+    event = senseHat.stick.wait_for_event() # forkorting av stick event
+
+    if event.direction == "left":  # hvis stikke på Hat trykkes mot venstre
+        start = time() # Finner startiden på logging
+        # Skriver over skrift til logg for å markere ny måling
+        write_to_log('\n\n### New measurements started at: ' + datatime.now().strftime('%d%m%Y %H:%M:%S' + ' ###\n'))
+        
+        while stick_down() != 1:  # kjører frem til stikke trykkes ned
+            p = calc_pressure_Pa() # henter kalkulert trykk
+            h = (T_1/a) * ( (p/p_1)**(-a*R/g_0) - 1) + h_1 # beregner høyde med hypsometrisk ligning
+            # finner tiden ved måling med å ta tiden minus starttid. Resulterer i tid etter start måling
+            now = time() - start
+            
+            # Skriver data hentet til log med tidsstempel.
+            write_to_log(str(now) + ',' + str(p) + ',' + str(h) + ',' + str(senseHat.temp))
+            
+            # Viser målt høyde avrundet i display med speed 0.03 (kommenteres ut hvis etasjemåling)
+            senseHat.show_message(str(round(h,2) )+ "m" ,0.03)
+            '''
+            draw(calcEtasjer())  # Printer etasjevisning (kan kommenteres ut hvis ikke ønsket)  
+            '''
+    
+    if event.direction == "right":  # Hvis stikke trykkes mot høyre
+        while stick down()) !=1: # Kjøerer frem til stikke trykkes ned
+            T_1 = senseHat.temp + 273.15 # finner temperatur og gjør om til grader celcius
+            p_1 = calc_pressure_Pa() # Finner kalkulert trykk
+             # viser målt høyde avrundet og temp i display med hastighet 0.03
+            senseHat.show_message(str(int (p_1))+" Pa "+str(round(T_1))+" K", 0.03)          
